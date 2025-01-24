e005c65ca55bcb322fe40ef3e453c7555c0315ac
Subigya Parajuli <subigyaparajulo009@gmail.com>
2025-01-24 15:15:24 -0800

    My first commit

---
+++ main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Activity one-Variable swap C++
+//
+//  Created by Subigya Parajuli on 1/24/25.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
