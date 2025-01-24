dac508e38bfe97bf062186d0c07bb26946fc7bcc
Subigya Parajuli <subigyaparajulo009@gmail.com>
2025-01-24 15:21:34 -0800

    Value Swapping between variables using C++

--- main.cpp
+++ main.cpp
@@ -6,9 +6,23 @@
 //
 
 #include <iostream>
+using namespace std;
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    
+    int a, b, temp;
+    
+    cout << "Please enter a number (input for a): ";
+    cin >> a;
+    
+    cout << "Please enter a second number (input for b): ";
+    cin >> b;
+    
+    temp = a;
+    a = b;
+    b = temp;
+    
+    cout << "Your swapped values are: a = " << a << " & b = " << b << endl;
+    
     return 0;
 }
